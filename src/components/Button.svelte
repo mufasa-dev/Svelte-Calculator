e43838f0309a5c34fd26fb14764d2d3ff28d558f
mufasa-dev <bruno.ribeiro96@hotmail.com.br>
2025-04-03 23:28:08 -0300

    customize button size

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
     export let text: string = "";
+    export let double: boolean = false;
+    export let triple: boolean = false;
 </script>
 
-<button class="btn">
+<button class="btn" class:triple class:double>
     {text}
 </button>
 
 <style>
     .btn {
+        flex-basis: 25%;
+    }
 
+    .double {
+        flex-basis: 50%;
+    }
+    .triple {
+        flex-basis: 75%;
     }
 </style>
--- src/components/Calculator.svelte
+++ src/components/Calculator.svelte
@@ -5,19 +5,31 @@
 
 <div class="calculator">
     <Line>
+        <Button triple text="AC" />
+        <Button text="X" />
+    </Line>
+    <Line>
         <Button text="7" />
         <Button text="8" />
         <Button text="9" />
+        <Button text="X" />
     </Line>
     <Line>
         <Button text="4" />
         <Button text="5" />
         <Button text="6" />
+        <Button text="-" />
     </Line>
     <Line>
         <Button text="3" />
         <Button text="2" />
         <Button text="1" />
+        <Button text="+" />
+    </Line>
+    <Line>
+        <Button double text="0" />
+        <Button text="," />
+        <Button text="=" />
     </Line>
 </div>
 
