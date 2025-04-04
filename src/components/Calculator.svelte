a723c9096338b614df2d81831b81e2faa832c656
mufasa-dev <bruno.ribeiro96@hotmail.com.br>
2025-04-03 23:11:16 -0300

    Add line component

--- src/components/Calculator.svelte
+++ src/components/Calculator.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
   import Button from "./Button.svelte";
+  import Line from "./Line.svelte";
 </script>
 
 <div class="calculator">
-    <Button text="7" />
-    <Button text="8" />
-    <Button text="9" />
+    <Line>
+        <Button text="7" />
+        <Button text="8" />
+        <Button text="9" />
+    </Line>
+    <Line>
+        <Button text="4" />
+        <Button text="5" />
+        <Button text="6" />
+    </Line>
+    <Line>
+        <Button text="3" />
+        <Button text="2" />
+        <Button text="1" />
+    </Line>
 </div>
 
 <style>
     .calculator {
-        background-color: --back;
+        background-color: var(--back);
         height: 220px;
         width: 150px;
         display: flex;
