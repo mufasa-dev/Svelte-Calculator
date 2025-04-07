dffff60927b232dd0479531499aa61a582b6c0ce
mufasa-dev <bruno.ribeiro96@hotmail.com.br>
2025-04-07 16:55:56 -0300

    Add 'public/origin/' from commit 'df0f57ad2e2072ac79669fea626d42ad153633e6'
    git-subtree-dir: public/origin
    git-subtree-mainline: df0f57ad2e2072ac79669fea626d42ad153633e6
    git-subtree-split: df0f57ad2e2072ac79669fea626d42ad153633e6

---
+++ public/origin/src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Calculator from "./components/Calculator.svelte";
+</script>
+
+<main>
+	<Calculator />
+</main>
+
+<style>
+	:global(body) {
+		background-color: #333;
+		padding: 0;
+	}
+	:root {
+		--back: #555;
+	}
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+</style>
---
+++ public/origin/src/components/Button.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    export let text: string = "";
+    export let double: boolean = false;
+    export let triple: boolean = false;
+    export let operation: boolean = false;
+    export let highlight: boolean = false;
+
+    export let onClick: (value:string) => void = () => {}
+</script>
+
+<button class="btn" 
+    on:click={() => onClick(text)}
+    class:triple class:double class:operation class:highlight>
+    {text}
+</button>
+
+<style>
+    .btn {
+        flex-basis: 25%;
+        margin: 0;
+        border: 2px solid var(--back);
+        border-radius: 5px;
+    }
+
+    .double {
+        flex-basis: 50%;
+    }
+    .triple {
+        flex-basis: 75%;
+    }
+
+    .operation {
+        background-color: orange;
+        color: white;
+    }
+    .operation:not(:disabled):active {
+        background-color: rgb(253, 173, 24);
+        color: white;
+    }
+
+    .highlight {
+        background: #b50000;
+        color: white;
+    }
+    .highlight:not(:disabled):active {
+        background: #d10505;
+    }
+</style>
